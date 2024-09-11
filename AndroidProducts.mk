bbf4516d8bb6763682e60ce89cd7453f275c197e
Ggo10086 <160459755+Ggo10086@users.noreply.github.com>
2024-09-11 19:11:50 +0800

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_on7xltechn.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_on7xltechn-user \
+    omni_on7xltechn-userdebug \
+    omni_on7xltechn-eng
